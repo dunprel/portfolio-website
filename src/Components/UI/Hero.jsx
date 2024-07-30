bf4f1e0c10e8584d92bc66a825d1b78d3e5df7b9
PRECIOUS DUNKWU <100343765+dunprel@users.noreply.github.com>
2024-07-30 08:06:27 +0100

    Years of experience updated.

--- src/Components/UI/Hero.jsx
+++ src/Components/UI/Hero.jsx
@@ -153,7 +153,7 @@ const Hero = () => {
             </div>
             <div className='mb-5'>
               <h2 className='text-headingColor font-[700] text-[32px]'>
-                <CountUp start={0} end={6} duration={2} suffix='+' />
+                <CountUp start={0} end={4} duration={2} suffix='+' />
               </h2>
               <h4 className='text-headingColor font-[600] text-[18px]'>
                 Years of Experience
